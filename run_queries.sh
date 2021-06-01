c4f1286d4f28ff13d1d88d0c8542dde802643fe9
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2021-06-01 11:22:10 +0200

    update queries

--- run_queries.sh
+++ run_queries.sh
@@ -10,18 +10,23 @@ trap "cleanup" EXIT
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Schweiz
-
 # WC's ganze Schweiz
 
 echo -ne "Query WC's ganze Schweiz...           "
-cat $DIR/queries/wc_switzerland.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/wc_switzerland.geojson
+cat $DIR/queries/toilets_switzerland.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_switzerland.geojson
+echo -ne "\t\t - Done.\r"
+echo ""
+
+# WC's ganz Deutschland
+
+echo -ne "Query WC's ganz Deutschland...           "
+cat $DIR/queries/toilets_germany.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_germany.geojson
 echo -ne "\t\t - Done.\r"
 echo ""
 
 # WC's weltweit
 
-echo -ne "Query WC's weltweit...           "
-cat $DIR/queries/wc_worldwide.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/wc_worldwide.geojson
-echo -ne "\t\t - Done.\r"
-echo ""
+#echo -ne "Query WC's weltweit...           "
+#cat $DIR/queries/toilets_worldwide.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_worldwide.geojson
+#echo -ne "\t\t - Done.\r"
+#echo ""
