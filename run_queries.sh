17cfc4f46b26497ab70ebc48882a3868384e6649
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2021-06-02 20:14:55 +0200

    typo

--- run_queries.sh
+++ run_queries.sh
@@ -12,49 +12,49 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # WC's ganze Schweiz
 
-echo -ne "Query WC's ganze Schweiz...           "
+echo -ne "Query Toiletten ganze Schweiz...           "
 cat $DIR/queries/toilets_switzerland.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_switzerland.geojson
 echo -ne "\t\t - Done.\r"
 echo ""
 
 # WC's ganz Deutschland
 
-echo -ne "Query WC's ganz Deutschland...           "
+echo -ne "Query Toiletten ganz Deutschland...           "
 cat $DIR/queries/toilets_germany.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_germany.geojson
 echo -ne "\t\t - Done.\r"
 echo ""
 
 # WC's ganz Österreich
 
-echo -ne "Query WC's ganz Oesterreich...           "
+echo -ne "Query Toiletten ganz Oesterreich...           "
 cat $DIR/queries/toilets_austria.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_austria.geojson
 echo -ne "\t\t - Done.\r"
 echo ""
 
 # WC's ganz Italien
 
-echo -ne "Query WC's ganz Italien...           "
+echo -ne "Query Toiletten ganz Italien...           "
 cat $DIR/queries/toilets_italy.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_italy.geojson
 echo -ne "\t\t - Done.\r"
 echo ""
 
 # WC's ganz Frankreich
 
-echo -ne "Query WC's ganz Frankreich...           "
+echo -ne "Query Toiletten ganz Frankreich...           "
 cat $DIR/queries/toilets_france.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_france.geojson
 echo -ne "\t\t - Done.\r"
 echo ""
 
 # WC's ganz Spanien
 
-echo -ne "Query WC's ganz Spanien...           "
+echo -ne "Query Toiletten ganz Spanien...           "
 cat $DIR/queries/toilets_spain.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_spain.geojson
 echo -ne "\t\t - Done.\r"
 echo ""
 
 # WC's ganze USA
 
-echo -ne "Query WC's ganze USA...           "
+echo -ne "Query Toiletten ganze USA...           "
 cat $DIR/queries/toilets_us.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_us.geojson
 echo -ne "\t\t - Done.\r"
 echo ""
