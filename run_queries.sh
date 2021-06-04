9b1d2662d78ea46107d40e6712695609d34fb081
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2021-06-04 12:52:16 +0200

    change overpass endpoint

--- run_queries.sh
+++ run_queries.sh
@@ -30,17 +30,3 @@ echo -ne "Query Toiletten ganz Oesterreich...           "
 cat $DIR/queries/toilets_austria.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_austria.geojson
 echo -ne "\t\t - Done.\r"
 echo ""
-
-# WC's ganz Italien
-
-echo -ne "Query Toiletten ganz Italien...           "
-cat $DIR/queries/toilets_italy.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_italy.geojson
-echo -ne "\t\t - Done.\r"
-echo ""
-
-# WC's ganz Italien
-
-echo -ne "Query Toiletten ganz Italien...           "
-cat $DIR/queries/toilets_italy.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_italy.geojson
-echo -ne "\t\t - Done.\r"
-echo ""
