61c088e6e2012ad3fd788772c121cc614ea132c6
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2021-06-04 12:56:58 +0200

    change overpass api instance

--- run_queries.sh
+++ run_queries.sh
@@ -30,3 +30,38 @@ echo -ne "Query Toiletten ganz Oesterreich...           "
 cat $DIR/queries/toilets_austria.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_austria.geojson
 echo -ne "\t\t - Done.\r"
 echo ""
+
+# WC's ganz Italien
+
+echo -ne "Query Toiletten ganz Italien...           "
+cat $DIR/queries/toilets_italy.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_italy.geojson
+echo -ne "\t\t - Done.\r"
+echo ""
+
+# WC's ganz Frankreich
+
+echo -ne "Query Toiletten ganz Frankreich...           "
+cat $DIR/queries/toilets_france.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_france.geojson
+echo -ne "\t\t - Done.\r"
+echo ""
+
+# WC's ganz Spanien
+
+echo -ne "Query Toiletten ganz Spanien...           "
+cat $DIR/queries/toilets_spain.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_spain.geojson
+echo -ne "\t\t - Done.\r"
+echo ""
+
+# WC's ganze USA
+
+echo -ne "Query Toiletten ganze USA...           "
+cat $DIR/queries/toilets_us.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_us.geojson
+echo -ne "\t\t - Done.\r"
+echo ""
+-------
+# WC's weltweit
+
+#echo -ne "Query WC's weltweit...           "
+#cat $DIR/queries/toilets_worldwide.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_worldwide.geojson
+#echo -ne "\t\t - Done.\r"
+#echo ""
