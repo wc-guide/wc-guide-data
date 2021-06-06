e5f1b90656afaa857a0138c5abdaa4add98bdb78
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2021-06-06 13:11:43 +0200

    update queries

--- run_queries.sh
+++ run_queries.sh
@@ -33,6 +33,11 @@ echo ""
 
 # WC's ganz Italien
 
+#echo -ne "Query Toiletten ganz Oesterreich...           "
+#cat $DIR/queries/toilets_italy.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_italy.geojson
+#echo -ne "\t\t - Done.\r"
+#echo ""
+
 # WC's ganz Frankreich
 
 #echo -ne "Query Toiletten ganz Frankreich...           "
