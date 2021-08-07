686a5f476511f65c9bdbb4f625ff5cd6e22013e8
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2021-08-07 21:30:09 +0200

    insert world test

--- run_queries.sh
+++ run_queries.sh
@@ -10,6 +10,13 @@ trap "cleanup" EXIT
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# WC's ganze Welt
+
+echo -ne "Query Toiletten ganz Oesterreich...           "
+cat $DIR/queries/toilets_world.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_world.geojson
+echo -ne "\t\t - Done.\r"
+echo ""
+
 # WC's ganz Österreich
 
 echo -ne "Query Toiletten ganz Oesterreich...           "
