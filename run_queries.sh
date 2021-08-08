1aded05280c49a0142154ca61ebc2b405ac00a1f
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2021-08-08 11:35:17 +0200

    insert taiwan

--- run_queries.sh
+++ run_queries.sh
@@ -318,6 +318,13 @@ cat $DIR/queries/toilets_south_korea.txt | python $DIR/overpass_query.py | osmto
 echo -ne "\t\t - Done.\r"
 echo ""
 
+# WC's ganz Taiwan
+
+echo -ne "Query Toiletten ganz Taiwan...           "
+cat $DIR/queries/toilets_taiwan.txt | python $DIR/overpass_query.py | osmtogeojson > $DIR/data/toilets_taiwan.geojson
+echo -ne "\t\t - Done.\r"
+echo ""
+
 # WC's ganzes Vereinigtes Königreich
 
 echo -ne "Query Toiletten ganzes Vereinigtes Königreich...           "
