4d01614bb335a6dbfe518b1c09d05b221fd8a700
murthy10 <samuel.kurath@gmail.com>
2021-08-08 18:22:09 +0200

    started to work on prepare data

---
+++ publish/prepare_data.py
@@ -0,0 +1,51 @@
+import argparse
+import json
+import os
+
+
+def get_type(properties):
+    result = 'normal'
+
+
+def supports_male(properties):
+    return properties.get("male", "no") == "yes"
+
+
+def supports_female(properties):
+    return properties.get("female", "no") == "yes"
+
+
+def has_urinal(properties):
+    return 'urinal' in properties.get('toilets:position', '')
+
+
+def is_type_iv(properties):
+    return properties.get("wheelchair", "no") == "yes"
+
+
+def create_feature(facility):
+    lon = facility['Standort']['X']
+    lat = facility['Standort']['Y']
+    operator = facility['Betreiber']['Name']
+    return {
+        "type": "Feature",
+        "id": None,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [float(lon), float(lat)]
+        },
+        "properties": {
+            "type": "eurokey",
+            "description": None,
+            "fee": None,
+            "name": None,
+            "operator": operator,
+            "access": False,
+            "features":
+                {
+                    "wickeltisch": False,
+                    "kostenpflichtig": False,
+                    "nettetoilette": False
+                },
+        }
+    }
